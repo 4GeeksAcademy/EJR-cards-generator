6a6385cc3614cfb7f0bc274dc3bf6b4d3456b8a6
Elías JR <167987056+eliasjr89@users.noreply.github.com>
2024-07-29 14:35:19 +0000

    card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,64 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let titulo = "Magic trik";
+document.title = titulo;
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  getcard();
 };
+
+function valorRandom() {
+  let numero = Math.ceil(Math.random() * 13);
+
+  if (numero == 1) {
+    numero = "A";
+  } else if (numero == 11) {
+    numero = "J";
+  } else if (numero == 12) {
+    numero = "Q";
+  } else if (numero == 13) {
+    numero = "K";
+  }
+  return numero;
+}
+
+function paloRandom() {
+  return Math.ceil(Math.random() * 4);
+}
+
+function getcard() {
+  document.querySelector("#numero").innerHTML = valorRandom();
+  switch (paloRandom()) {
+    case 1:
+      document.querySelector("#carta").className = "card";
+      document.querySelector("#carta").classList.add("diamante");
+      document.querySelector("#paloTop").innerHTML = "♦";
+      document.querySelector("#paloBot").innerHTML = "♦";
+      break;
+    case 2:
+      document.querySelector("#carta").className = "card";
+      document.querySelector("#carta").classList.add("corazon");
+      document.querySelector("#paloTop").innerHTML = "♥";
+      document.querySelector("#paloBot").innerHTML = "♥";
+      break;
+    case 3:
+      document.querySelector("#carta").className = "card";
+      document.querySelector("#carta").classList.add("pica");
+      document.querySelector("#paloTop").innerHTML = "♠";
+      document.querySelector("#paloBot").innerHTML = "♠";
+      break;
+    case 4:
+      document.querySelector("#carta").className = "card";
+      document.querySelector("#carta").classList.add("trebol");
+      document.querySelector("#paloTop").innerHTML = "♣";
+      document.querySelector("#paloBot").innerHTML = "♣";
+      break;
+  }
+}
+
+document.getElementById("btn1").addEventListener("click", () => {
+  getcard();
+});
